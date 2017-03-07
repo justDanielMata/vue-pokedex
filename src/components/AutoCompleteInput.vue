d4292b59029347c8a342e857d8bbfe8ff7d96621
daniel mata <daniel.mata@fdvsolutions.com>
2017-03-07 14:21:54 -0300

    added component

---
+++ src/components/AutoCompleteInput.vue
@@ -0,0 +1,19 @@
+<template>
+ <input class='form-control' type='text' v-model.trim='input' @change='changeOption'/>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: '',
+    }
+  },
+  props: [ 'options' ],
+  methods: {
+    changeOption() {
+      this.$emit('inputChanged', this.input);
+    }
+  }
+}
+</script>
