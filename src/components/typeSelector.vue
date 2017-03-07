169e6b861b3b5a18c76d355bca414909729a012d
daniel mata <daniel.mata@fdvsolutions.com>
2017-03-07 11:32:18 -0300

    added the resource to get pokemon by type

--- src/components/typeSelector.vue
+++ src/components/typeSelector.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <span v-for='pType in pokemonTypes'>
-    <button :class='[btn, pType.name]' style="margin:0.5em;">{{ pType.name }}</button>
+    <button :class='[btn, pType.name]' style="margin:0.5em;" @click='getPokemonByType(pType.name)'>{{ pType.name }}</button>
   </span>
 </div>
 </template>
@@ -10,10 +10,27 @@
 export default {
   data() {
     return {
-      btn: 'btn'
+      btn: 'btn',
+      pokemonByType: [],
       }
   },
   props:[ 'pokemonTypes' ],
+  methods: {
+    getPokemonByType(name) {
+      this.resource.pokemonByType({type: name})
+        .then( response => { return response.json(); })
+        .then( data => {
+          return this.pokemonByType = data.pokemon;
+        })
+    } 
+  },
+  
+  created() {
+    const customActions = {
+      pokemonByType: { method: 'GET' }
+    };
+    this.resource = this.$resource('type/{type}/', {}, customActions);
+  }
 }
 </script>
 
